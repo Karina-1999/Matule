17fdf7fac6a085867dab2d46b12854e478b6987a
Karina-1999 <phantom.kot2007@yandex.ru>
2026-09-11 09:56:37 +0300

    Написание библиотеки UIKit

---
+++ UIKit_lybrary/src/androidTest/java/com/example/uikit_lybrary/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.uikit_lybrary.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+    val accent = Color(0xFF2074F2)
+    val accentInactive = Color(0xFFC5D2FF)
+    val black = Color(0xFF2D2C2C)
+    val white = Color(0xFFF7F7F7)
+    val error = Color(0xFFFF4646)
+    val success = Color(0xFF00B412)
+    val inputBackground = Color(0xFFF7F7FA)
+    val inputStroke = Color(0xFFE6E6E6)
+    val inputIcon = Color(0xFFBFC7D1)
+    val placeholder = Color(0xFF98989A)
+    val description = Color(0xFF8787A1)
+    val cardStroke = Color(0xFFF2F2F2)
